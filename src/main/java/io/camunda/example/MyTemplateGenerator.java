8ecf9b3a921abea132ec1660d8614ec457e21f3a
Pavel Derendyaev <dddpaul@gmail.com>
2024-01-06 16:30:12 +0300

    Write generated template to file

--- src/main/java/io/camunda/example/MyTemplateGenerator.java
+++ src/main/java/io/camunda/example/MyTemplateGenerator.java
@@ -1,12 +1,22 @@
 package io.camunda.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.connector.generator.dsl.OutboundElementTemplate;
 import io.camunda.connector.generator.java.OutboundClassBasedTemplateGenerator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MyTemplateGenerator {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         OutboundClassBasedTemplateGenerator generator = new OutboundClassBasedTemplateGenerator();
         OutboundElementTemplate template = generator.generate(MyConnectorFunction.class);
+        try (FileWriter writer = new FileWriter("element-templates/template-connector.json")) {
+            writer.write(new ObjectMapper()
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(template));
+        }
     }
 }
